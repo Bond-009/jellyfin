2185ff963887e94641274e81a49985f56f89d90f
Bond_009 <bond.009@outlook.com>
2024-08-05 17:36:34 +0200

    Stub out DirectoryService

--- MediaBrowser.Controller/Providers/DirectoryService.cs
+++ MediaBrowser.Controller/Providers/DirectoryService.cs
@@ -1,115 +1,55 @@
 #pragma warning disable CS1591
 
-using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using MediaBrowser.Model.IO;
 
-namespace MediaBrowser.Controller.Providers
+namespace MediaBrowser.Controller.Providers;
+
+public class DirectoryService : IDirectoryService
 {
-    public class DirectoryService : IDirectoryService
+    private readonly IFileSystem _fileSystem;
+
+    public DirectoryService(IFileSystem fileSystem)
     {
-        private readonly IFileSystem _fileSystem;
+        _fileSystem = fileSystem;
+    }
 
-        private readonly ConcurrentDictionary<string, FileSystemMetadata[]> _cache = new(StringComparer.Ordinal);
+    public FileSystemMetadata[] GetFileSystemEntries(string path)
+        => _fileSystem.GetFileSystemEntries(path).ToArray();
 
-        private readonly ConcurrentDictionary<string, FileSystemMetadata> _fileCache = new(StringComparer.Ordinal);
+    public List<FileSystemMetadata> GetDirectories(string path)
+        => _fileSystem.GetDirectories(path).ToList();
 
-        private readonly ConcurrentDictionary<string, List<string>> _filePathCache = new(StringComparer.Ordinal);
+    public List<FileSystemMetadata> GetFiles(string path)
+        => _fileSystem.GetFiles(path).ToList();
 
-        public DirectoryService(IFileSystem fileSystem)
+    public FileSystemMetadata? GetFile(string path)
+        => _fileSystem.GetFileInfo(path);
+
+    public FileSystemMetadata? GetDirectory(string path)
+        => _fileSystem.GetDirectoryInfo(path);
+
+    public FileSystemMetadata? GetFileSystemEntry(string path)
+        => _fileSystem.GetFileSystemInfo(path);
+
+    public IReadOnlyList<string> GetFilePaths(string path)
+        => GetFilePaths(path, false);
+
+    public IReadOnlyList<string> GetFilePaths(string path, bool clearCache, bool sort = false)
+    {
+        var filePaths = _fileSystem.GetFilePaths(path);
+
+        if (sort)
         {
-            _fileSystem = fileSystem;
+            filePaths.Order();
         }
 
-        public FileSystemMetadata[] GetFileSystemEntries(string path)
-        {
-            return _cache.GetOrAdd(path, static (p, fileSystem) => fileSystem.GetFileSystemEntries(p).ToArray(), _fileSystem);
-        }
+        return filePaths.ToList();
+    }
 
-        public List<FileSystemMetadata> GetDirectories(string path)
-        {
-            var list = new List<FileSystemMetadata>();
-            var items = GetFileSystemEntries(path);
-            for (var i = 0; i < items.Length; i++)
-            {
-                var item = items[i];
-                if (item.IsDirectory)
-                {
-                    list.Add(item);
-                }
-            }
-
-            return list;
-        }
-
-        public List<FileSystemMetadata> GetFiles(string path)
-        {
-            var list = new List<FileSystemMetadata>();
-            var items = GetFileSystemEntries(path);
-            for (var i = 0; i < items.Length; i++)
-            {
-                var item = items[i];
-                if (!item.IsDirectory)
-                {
-                    list.Add(item);
-                }
-            }
-
-            return list;
-        }
-
-        public FileSystemMetadata? GetFile(string path)
-        {
-            var entry = GetFileSystemEntry(path);
-            return entry is not null && !entry.IsDirectory ? entry : null;
-        }
-
-        public FileSystemMetadata? GetDirectory(string path)
-        {
-            var entry = GetFileSystemEntry(path);
-            return entry is not null && entry.IsDirectory ? entry : null;
-        }
-
-        public FileSystemMetadata? GetFileSystemEntry(string path)
-        {
-            if (!_fileCache.TryGetValue(path, out var result))
-            {
-                var file = _fileSystem.GetFileSystemInfo(path);
-                if (file?.Exists ?? false)
-                {
-                    result = file;
-                    _fileCache.TryAdd(path, result);
-                }
-            }
-
-            return result;
-        }
-
-        public IReadOnlyList<string> GetFilePaths(string path)
-            => GetFilePaths(path, false);
-
-        public IReadOnlyList<string> GetFilePaths(string path, bool clearCache, bool sort = false)
-        {
-            if (clearCache)
-            {
-                _filePathCache.TryRemove(path, out _);
-            }
-
-            var filePaths = _filePathCache.GetOrAdd(path, static (p, fileSystem) => fileSystem.GetFilePaths(p).ToList(), _fileSystem);
-
-            if (sort)
-            {
-                filePaths.Sort();
-            }
-
-            return filePaths;
-        }
-
-        public bool IsAccessible(string path)
-        {
-            return _fileSystem.GetFileSystemEntryPaths(path).Any();
-        }
+    public bool IsAccessible(string path)
+    {
+        return _fileSystem.GetFileSystemEntryPaths(path).Any();
     }
 }
